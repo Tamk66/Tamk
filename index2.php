d650b8127951ad67584c0104a8220e08670ec569
Tanyuke <654499434@qq.com>
2022-09-15 19:08:35 +0800

    222

---
+++ index2.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 3N
+ * Date: 2022/9/15
+ * Time: 15:01
+ */
+
+namespace test;
+
+
+class index2
+{
+    public function index()
+    {
+        do {
+
+        } while (true);
+
+        while (true) {
+            // structure body
+        }
+
+        for ($i = 0; $i < 10; $i++) {
+            // for body
+        }
+
+        foreach ($iterable as $key => $value) {
+            // foreach body
+        }
+
+        try {
+            // try body
+        } catch (FirstExceptionType $e) {
+            // catch body
+        } catch (OtherExceptionType $e) {
+            // catch body
+        }
+    }
+
+}
